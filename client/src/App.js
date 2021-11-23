394a8658e66137a1d5e42a33e35c3c8288827b15
toanmimo <toanmimo@jyu.fi>
2021-11-23 10:54:28 +0200

    Muutoksia

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import './app.css'
+import kuva from './images/kytkenta.jpg'
 import noteService from './services/service'
 
 const App = () => {
@@ -15,7 +17,20 @@ const App = () => {
 
   return (
     <div className="App">
-      <p>testi</p>
+      <p>1. <a href='https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/python-setup' >Mittarin ohjeet</a></p>
+      <p>Kytkentäkaavio:</p>
+      <img src={kuva}></img>
+      <p>2. <a href='https://docs.python-requests.org/en/latest/' >Python requests</a></p>
+      <p>Get polkuja:<br/>/api/temperatures<br/>/api/latestbyid<br/><br/>
+      Post polku:<br/>/api/posttemperature<br/><br/>
+	    Muotoa:
+	    <br/><br/>
+      {'{'}<br/>
+		  <pre>   "temp": float,<br/></pre>
+		  <pre>   "apikey": string,<br/></pre>
+		  <pre>   "deviceId": int<br/></pre>
+      {'}'}<br/><br/>
+      Datan pitää olla json muodossa!</p>
     </div>
   );
 }
